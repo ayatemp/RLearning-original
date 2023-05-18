3f1eb222669e50f55032bc1864704f14e7289260
ayato_kaku <ayato.kaku@gmail.com>
2023-05-18 14:27:53 +0900

    no message

--- ch07/dezero3_original.py
+++ ch07/dezero3_original.py
@@ -13,3 +13,28 @@ b = Variable(np.zeros(1))       #バイアス
 def predict(x):
     y = F.matmul(x,W) + b
     return y
+
+def mean_squared_error(x0,x1):
+    diff = x0 - x1
+    return F.sum(diff**2) / len(diff)
+
+lr = 0.1
+iters = 100
+
+for i in range(iters):
+    y_pred = predict(x)
+    loss = mean_squared_error(y,y_pred) 
+
+    W.cleargrad()
+    b.cleargrad()
+    loss.backward() #勾配を計算し自動で逆転バイしてくれている
+
+    W.data -= lr*W.grad.data #ここで重みとバイアスを更新している
+    b.data -= lr*b.grad.data
+
+    if i % 10 == 0:
+        print(loss.data)
+    
+print('====')
+print('W =', W.data)
+print('b =', b.data)
