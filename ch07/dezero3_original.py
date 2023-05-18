f72fb829f9fc1a5bd625b90ef7a0b461642963e3
ayato_kaku <ayato.kaku@gmail.com>
2023-05-18 14:36:25 +0900

    add plt for graph

--- ch07/dezero3_original.py
+++ ch07/dezero3_original.py
@@ -1,6 +1,7 @@
 import numpy as np
 from dezero import Variable
 import dezero.functions as F
+import matplotlib.pyplot as plt
 
 np.random.seed(0)
 x = np.random.rand(100,1)
@@ -21,6 +22,9 @@ def mean_squared_error(x0,x1):
 lr = 0.1
 iters = 100
 
+loss_history = []
+
+
 for i in range(iters):
     y_pred = predict(x)
     loss = mean_squared_error(y,y_pred) 
@@ -32,9 +36,27 @@ for i in range(iters):
     W.data -= lr*W.grad.data #ここで重みとバイアスを更新している
     b.data -= lr*b.grad.data
 
+    loss_history.append(loss.data) #lossの履歴を保存している
+
     if i % 10 == 0:
         print(loss.data)
     
+
+plt.plot(loss_history)
+plt.xlabel("iteration")
+plt.ylabel("loss")
+plt.title("Loss history")
+plt.show()
+
+# グラフの描画
+plt.scatter(x.data, y.data, label='Data')
+plt.plot(x.data, y_pred.data, color='r', label='Prediction')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Data and Prediction')
+plt.legend()
+plt.show()
+
 print('====')
 print('W =', W.data)
 print('b =', b.data)
